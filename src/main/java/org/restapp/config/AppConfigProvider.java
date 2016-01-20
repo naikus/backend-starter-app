05d3408e9077004bdc60800a41e76b78f2df12c0
Aniket Naik <aniketn3@gmail.com>
2016-01-20 15:57:15 +0530

    Minor enhancements to configuration api

--- src/main/java/org/restapp/config/AppConfigProvider.java
+++ src/main/java/org/restapp/config/AppConfigProvider.java
@@ -16,12 +16,12 @@ import org.slf4j.LoggerFactory;
 public class AppConfigProvider extends ConfigProvider {
   private static final Logger LOG = LoggerFactory.getLogger(AppConfigProvider.class.getSimpleName());
   public static final String CONFIG_FILE = "app.properties";
-  public static final String ENV_PREFIX = "restapp";
+  public static final String ENV_PREFIXES = "restapp";
   
   private Configuration configuration;
   
   public AppConfigProvider() {
-    
+  
   }
 
   @Override
@@ -34,25 +34,38 @@ public class AppConfigProvider extends ConfigProvider {
   
   private Configuration load() {
     // load from app.properties
-    Configuration fileConfig = loadFromProperties();
+    Configuration fileConfig = loadFromConfigFile();
     // check prefix in app.properties and then load from env
-    String prefix = fileConfig.getString("iotvertical.env.prefix");
-    if(prefix == null || prefix.trim().length() == 0) {
-      prefix = ENV_PREFIX;
+    String prefixes = fileConfig.getString("config.env.prefixes");
+    if(prefixes == null || prefixes.trim().length() == 0) {
+      prefixes = ENV_PREFIXES;
     }
-    Configuration envConfig = loadFromEnv(prefix);
-    // merge
+    
+    Configuration envConfig = new Configuration(),
+        sysPropsConfig = new Configuration();
+    for(String prefix : prefixes.split("\\s")) {
+      // merge
+      envConfig.merge(loadFromEnv(prefix));
+      sysPropsConfig.merge(loadFromProperties(prefix));
+    }
+    LOG.info("Loaded environment properties \n{}", envConfig);
+    LOG.info("Loaded system properties \n{}", sysPropsConfig);
     fileConfig.merge(envConfig);
+    fileConfig.merge(sysPropsConfig);
     return fileConfig;
   }
   
-  private Configuration loadFromProperties() {
+  private Configuration loadFromConfigFile() {
     InputStream in = null;
     Configuration conf = null;
     try {
       in = Thread.currentThread().getContextClassLoader().getResourceAsStream(CONFIG_FILE);
       Properties props = new Properties();
-      props.load(in);
+      if(in != null) {
+        props.load(in);
+      }else {
+        LOG.warn("Configuration file {} not found. Using defaults", CONFIG_FILE);
+      }
       conf = new Configuration(props);
     }catch(IOException e) {
       LOG.error("Cannot load configuration from" + CONFIG_FILE, e);
@@ -63,9 +76,24 @@ public class AppConfigProvider extends ConfigProvider {
     return conf;
   }
   
+  public Configuration loadFromProperties(String configKeyPrefix) {
+    Properties env = System.getProperties();
+    LOG.debug("Loading system properties with prefix {}", configKeyPrefix);
+    
+    Map<String, Object> envMap = new HashMap<>();
+    env.forEach((Object key, Object value) -> {
+      String k = key.toString();
+      if(k.startsWith(configKeyPrefix + ".")) {
+        envMap.put(k, value);
+      }
+    });
+    return new Configuration(envMap);
+  }
+  
   private Configuration loadFromEnv(String configKeyPrefix) {
     Map<String, String> env = System.getenv();
     Map<String, Object> envProps = new HashMap<>();
+    LOG.debug("Loading environment properties with prefix {}", configKeyPrefix);
     env.entrySet().stream()
         .filter((Map.Entry<String, String> e) -> {
           String key = e.getKey();
@@ -75,7 +103,6 @@ public class AppConfigProvider extends ConfigProvider {
             String key = e.getKey().replace("_", ".").toLowerCase();
             envProps.put(key, e.getValue());
         });
-    LOG.info("Loaded environment properties {}", envProps);
     return new Configuration(envProps);
   }
   
@@ -85,5 +112,18 @@ public class AppConfigProvider extends ConfigProvider {
     }catch(IOException ioe) {
       // ignore
     }
+  }
+  
+  @SuppressWarnings("unchecked")
+  public static Configuration from(Map config) {
+    Map<String, Object> map = new HashMap<>();
+    config.forEach((k, v) -> {
+      map.put(k == null ? null : k.toString(), v);
+    });
+    return new Configuration(map);
+  }
+  
+  public static Configuration from(Properties config) {
+    return new Configuration(config);
   }
 }
